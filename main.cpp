41882b8817512b148db7c90b7e0256e809e07774
SennovIY <SennovIY@mpei.ru>
2022-04-17 21:19:11 +0300

    Создан проект для модульных тестов, созданы тесты find_minmax на +,-,same,alone,empty

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include "histogram.h"
+
 #include <iostream>
 #include <vector>
 
@@ -9,19 +11,6 @@ vector<double> input_numbers(size_t count) {
         cin >> result[i];
     }
     return result;
-}
-
-void find_minmax(const vector<double>& numbers, double& min, double& max) {
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
 }
 
 vector<size_t> make_histogram(const vector<double>& numbers, size_t bin_count) {
