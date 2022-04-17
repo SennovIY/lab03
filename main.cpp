9c1142666aa3ba892c6833aac3c8130c63cdabb7
SennovIY <SennovIY@mpei.ru>
2022-04-17 20:13:38 +0300

    code: оформление гистограммы в SVG

--- main.cpp
+++ main.cpp
@@ -70,6 +70,49 @@ void show_histogram_text(const vector<size_t>& bins) {
     }
 }
 
+void svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_end() {
+    cout << "</svg>\n";
+}
+
+void svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
+}
+
+void svg_rect(double x, double y, double width, double height, string fill = "black", string stroke = "black") {
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "'/>";
+}
+
+void show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    const auto BIN_STROKE = "red";
+    const auto BIN_FILL = "#3CB371";
+
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+    for (size_t bin : bins) {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, BIN_FILL, BIN_STROKE);
+        top += BIN_HEIGHT;
+    }
+    svg_end();
+}
+
 int main() {
     // Ввод данных
     size_t number_count;
@@ -87,7 +130,7 @@ int main() {
     const auto bins = make_histogram(numbers, bin_count);
 
     // Вывод данных
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
 
     return 0;
 }
