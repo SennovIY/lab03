5c0fb8cfdfaf9ae25d5af7cc6e8ba3153c065cea
SennovIY <SennovIY@mpei.ru>
2022-05-29 11:56:37 +0300

    code: функции read_input() добавлен новый параметр bool prompt, отвечающий за вывод подсказок при вводе данных

--- main.cpp
+++ main.cpp
@@ -20,17 +20,23 @@ vector<double> input_numbers(istream& in, size_t count) {
 }
 
 Input
-read_input(istream& in) {
+read_input(istream& in, bool prompt = "true") {
     Input data;
 
-    cerr << "Enter number count: ";
+    if (prompt) {
+        cerr << "Enter number count: ";
+    }
     size_t number_count;
     in >> number_count;
 
-    cerr << "Enter numbers: ";
+    if (prompt) {
+        cerr << "Enter numbers: ";
+    }
     data.numbers = input_numbers(in, number_count);
 
-    cerr << "Enter column count: ";
+    if (prompt) {
+        cerr << "Enter column count: ";
+    }
     in >> data.bin_count;
 
     return data;
@@ -84,7 +90,7 @@ void show_histogram_text(const vector<size_t>& bins) {
 
 int main() {
     // ¬вод данных
-    const auto input = read_input(cin);
+    const auto input = read_input(cin, true);
 
     // ќбработка данных
     const auto bins = make_histogram(input);
