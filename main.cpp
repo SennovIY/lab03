5265c0cb699fa9b310eb4875f8bec30aed708d75
SennovIY <SennovIY@mpei.ru>
2022-05-29 11:04:47 +0300

    code: ввод данных осуществляется в структуру Input

--- main.cpp
+++ main.cpp
@@ -6,12 +6,34 @@
 
 using namespace std;
 
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
+
 vector<double> input_numbers(istream& in, size_t count) {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++) {
         cin >> result[i];
     }
     return result;
+}
+
+Input
+read_input(istream& in) {
+    Input data;
+
+    cerr << "Enter number count: ";
+    size_t number_count;
+    in >> number_count;
+
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
+
+    cerr << "Enter column count: ";
+    in >> data.bin_count;
+
+    return data;
 }
 
 vector<size_t> make_histogram(const vector<double>& numbers, size_t bin_count) {
@@ -62,19 +84,10 @@ void show_histogram_text(const vector<size_t>& bins) {
 
 int main() {
     // Ввод данных
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
-
-    cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(cin, number_count);
-
-    size_t bin_count;
-    cerr << "Enter column count: ";
-    cin >> bin_count;
+    const auto input = read_input(cin);
 
     // Обработка данных
-    const auto bins = make_histogram(numbers, bin_count);
+    const auto bins = make_histogram(input.numbers, input.bin_count);
 
     // Вывод данных
     show_histogram_svg(bins);
